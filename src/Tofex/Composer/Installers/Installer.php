78ec39e67e9767fb95ea728985ac51edf4a8c137
Andreas Knollmann <aknollmann@tofex.de>
2022-05-02 20:22:09 +0200

    added debugging in update

--- src/Tofex/Composer/Installers/Installer.php
+++ src/Tofex/Composer/Installers/Installer.php
@@ -42,23 +42,8 @@ class Installer
      */
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
-        echo sprintf("Update Repo: %s\n", $repo->getRepoName());
-        echo sprintf("Update Initial Name: %s\n", $initial->getName());
-        echo sprintf("Update Initial Binaries: \n%s\n", implode("\n", $initial->getBinaries()));
+        echo sprintf("Update path: %s\n", $this->getInstallPath($initial));
 
         return parent::update($repo, $initial, $target);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        echo sprintf("Uninstall Repo: %s\n", $repo->getRepoName());
-        echo sprintf("Uninstall Package Name: %s\n", $package->getName());
-        echo sprintf("Uninstall Package Version: %s\n", $package->getVersion());
-        echo sprintf("Uninstall Package Binaries: \n%s\n", implode("\n", $package->getBinaries()));
-
-        return parent::uninstall($repo, $package);
     }
 }
